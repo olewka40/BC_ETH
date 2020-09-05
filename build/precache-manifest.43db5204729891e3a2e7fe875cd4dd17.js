25baf6d4e347c5d703f0a1c69d04699071a0eec8
Oleg <oleg-kaluga2001@mail.ru>
2020-09-05 13:21:47 +0300

    123

---
+++ build/precache-manifest.43db5204729891e3a2e7fe875cd4dd17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65cb276b19bf635ae070",
+    "url": "/static/js/main.65cb276b.chunk.js"
+  },
+  {
+    "revision": "bacf49050715ea5402fd",
+    "url": "/static/css/1.bad3ad88.chunk.css"
+  },
+  {
+    "revision": "bacf49050715ea5402fd",
+    "url": "/static/js/1.bacf4905.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4955d5d9c596a0b153e357257398958c",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { FunctionsContext } from "./context/FunctionsContext";
+
+export const Navbar = () => {
+  const { account } = useContext(FunctionsContext);
+  return (
+    <StyledHeader>
+      <Text>Test project</Text>
+      <Text>Eth wallet: {account}</Text>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.div`
+  height: 48px;
+  width: 100%;
+  background-color: #9fcdff;
+  display: flex;
+  flex-direction: column;
+`;
+const Text = styled.div`
+  color: black;
+  font-size: 16px;
+`;
